d8ebc877c2ade44ce5eeee39c105ca63274165fd
Shay Dahan <shay1999d@gmail.com>
2020-06-20 19:38:51 +0300

    Lets get it started

---
+++ gitlab-yaml.py
@@ -0,0 +1,75 @@
+from job import Job
+import yaml
+
+
+class GitlabYAML:
+    def __init__(self):
+        self.instructions = {
+            'image': {},
+            'stages': [],
+        }
+
+        self.jobs = {}
+        self.default = None
+        self.active_stage = None
+        self.active_job = None
+
+    def clear_unused_keys(self, obj):
+        removable = [[], None, {}, '']
+        if isinstance(obj, dict):
+            return {k: self.clear_unused_keys(v) for k, v in obj.items() if v not in removable}
+        elif  isinstance(obj, Job):
+            return {k: v for k, v in obj.__dict__.items() if v not in removable}
+        else:
+            return obj
+
+    def build(self):
+        if self.jobs:
+            self.instructions.update(self.jobs)
+        self.instructions = self.clear_unused_keys(self.instructions)
+
+        print(self.instructions)
+        return yaml.dump(self.instructions, default_flow_style=False)
+
+    def image(self, name=None, entrypoint=None, command=None, alias=None):
+        if name:
+            self.instructions['image']['name'] = name
+        if entrypoint:
+            self.instructions['image']['entrypoint'] = entrypoint
+        if command:
+            self.instructions['image']['command'] = command
+        if alias:
+            self.instructions['image']['alias'] = alias
+        return self
+
+    def stage(self, stage_name):
+        self.active_stage = stage_name
+        if stage_name not in self.instructions['stages']:
+            self.instructions['stages'].append(stage_name)
+        return self
+
+    def job(self, job_name):
+        self.active_job = job_name
+        self.jobs[job_name] = Job(job_name, self.active_stage)
+        return self
+
+    def script(self, command):
+        if isinstance(self.jobs['staging'], Job):
+            self.jobs[self.active_job].add_script(command)
+        return self
+
+    def before_script(self, command):
+        return self
+
+    def after_script(self, command):
+        return self
+
+
+if __name__ == '__main__':
+    config = GitlabYAML()
+    # config.image('ruby:4.0.1')
+    build_stage = config.stage('deploy')
+    build_stage.job('staging').script('echo test').script('test code').script('sup man').after_script(
+        'hello user').before_script('sup')
+
+    config.build()
---
+++ job.py
@@ -0,0 +1,20 @@
+class Job:
+    def __init__(self, name, stage):
+        self.name = name
+        self.stage = stage
+        self.before_scripts = []
+        self.scripts = []
+
+        self.after_scripts = []
+
+    def add_script(self, command):
+        self.scripts.append(command)
+
+    def add_before_script(self, command):
+        self.before_scripts.append(command)
+
+    def add_after_script(self, command):
+        self.after_scripts.append(command)
+
+    def only(self, branch):
+        pass
